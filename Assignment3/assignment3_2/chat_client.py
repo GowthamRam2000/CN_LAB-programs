1461d6f55d9db0b33e5b00b3be0678c0e624fbfd
Gowthamram <40654487+GowthamRam2000@users.noreply.github.com>
2020-11-18 21:48:16 +0530

    Update chat_client.py

--- Assignment3/assignment3_2/chat_client.py
+++ Assignment3/assignment3_2/chat_client.py
@@ -1,34 +1,35 @@
-# Python program to implement client side of chat room. 
-import socket 
-import select 
-import sys 
+import socket
+import threading
+nickname = input("Choose your nickname: ")
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-if len(sys.argv) != 3: 
-	print ("Correct usage: script, IP address, port number")
-	exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("An error occured!")
+            client.close()
+            break
+# Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
 
-while True: 
-
-	
-	sockets_list = [sys.stdin, server] 
-
-	
-	read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-
-	for socks in read_sockets: 
-		if socks == server: 
-			message = socks.recv(2048) 
-			print message 
-		else: 
-			message = sys.stdin.readline() 
-			server.send(message) 
-			sys.stdout.write("<You>") 
-			sys.stdout.write(message) 
-			sys.stdout.flush() 
-server.close() 
+write_thread = threading.Thread(target=write)
+write_thread.start()
 
 
